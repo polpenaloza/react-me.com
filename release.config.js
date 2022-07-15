c4659bc0406a0c7687186f18b78882eb75999c20
polpenaloza <polpenaloza@gmail.com>
2022-07-15 12:28:01 +0200

    fix semantic

--- release.config.js
+++ release.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-template-curly-in-string */
 module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
@@ -12,5 +11,11 @@ module.exports = {
     ['@semantic-release/github', { assets: [{ path: 'docs/CHANGELOG.md' }] }],
     '@semantic-release/git',
   ],
-  branches: ['main'],
+  branches: [
+    { name: '1.x', range: '1.x', channel: '1.x' }, // Only after the `1.x` is created in the repo
+    { name: '2.x', range: '2.x', channel: '2.x' }, // Only after the `2.x` is created in the repo
+    { name: 'beta', channel: 'beta', prerelease: true }, // `prerelease` is set to `beta` as it is the value of `name`
+    { name: 'main' },
+  ],
+  // dryRun: true,
 }
