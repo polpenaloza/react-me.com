6a2bebb644cbf1cc796b9f6ed43e26f687cb9346
polpenaloza <polpenaloza@gmail.com>
2022-11-14 13:29:46 +0100

    rm i18n

--- next.config.js
+++ next.config.js
@@ -6,15 +6,11 @@ const nextConfig = {
   devIndicators: {
     buildActivityPosition: 'bottom-right',
   },
-  i18n: {
-    locales: ['en-US', 'es-ES'],
-    defaultLocale: 'en-US',
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      ...defaultPathMap,
+    }
   },
-  // exportPathMap: async function (defaultPathMap) {
-  //   return {
-  //     ...defaultPathMap,
-  //   }
-  // },
 }
 
 module.exports = nextConfig
