0c772c0649b2a7a8d5110fad27dd0214d78bdbfc
Pablo Penaloza <polpenaloza@gmail.com>
2023-01-13 18:05:34 +0100

    feat(bday): salute (#80)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,14 @@ const Home = () => {
           className='animate__animated animate__fadeIn  p-3 text-3xl font-bold'
           color={darkMode ? 'purple' : 'deeppink'}
           pause={3000}
-          items={[t('welcome'), t('howAreYou'), t('slang')]}
+          items={[
+            t('happyBirthday'),
+            t('happyBirthday5'),
+            t('happyBirthday2'),
+            t('happyBirthday3'),
+            t('happyBirthday4'),
+            t('happyBirthday6'),
+          ]}
         />
         <div
           className={classNames([
