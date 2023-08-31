bed8acd72518467877ef3ad48b61aa6e75898d3b
polpenaloza <polpenaloza@gmail.com>
2023-08-31 23:54:59 +0200

    fixes

--- src/components/Icon/GithubIcon.tsx
+++ src/components/Icon/GithubIcon.tsx
@@ -2,8 +2,8 @@ import { mdiGithub } from '@mdi/js'
 
 import BaseIcon, { BaseProps } from './BaseIcon'
 
-export const TwitterIcon = (props: Omit<BaseProps, 'path'>) => {
+export const GithubIcon = (props: Omit<BaseProps, 'path'>) => {
   return <BaseIcon {...props} path={mdiGithub} />
 }
 
-export default TwitterIcon
+export default GithubIcon
