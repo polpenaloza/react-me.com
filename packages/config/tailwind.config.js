1b4fc38fbed4e30709b2f9a97cfd17d17be6e598
polpenaloza <polpenaloza@gmail.com>
2024-07-28 21:19:23 +0200

    fix: crap 2

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -6,8 +6,14 @@ function setupConfig(props = {}) {
   return {
     ...props,
     content: [
-      '../../packages/ui/**/*.{js,ts,jsx,tsx}',
-      './src/**/*.{js,ts,jsx,tsx}',
+      // './**/*.{js,ts,jsx,tsx,mdx}',
+      './src/**/*.{js,ts,jsx,tsx,mdx}',
+      // './app/**/*.{js,ts,jsx,tsx,mdx}',
+      // './pages/**/*.{js,ts,jsx,tsx,mdx}',
+      // './components/**/*.{js,ts,jsx,tsx,mdx}',
+      // './providers/**/*.{js,ts,jsx,tsx,mdx}',
+      // '../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}',
+      '../.././node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
       './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
